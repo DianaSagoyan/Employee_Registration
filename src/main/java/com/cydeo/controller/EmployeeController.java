7df66ef5e7c4b7c9a45962c8c285e196f7815f8b
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-26 11:44:45 -0500

    Finished the UI part

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EmployeeController {
+    @RequestMapping("employee")
     public String employeePage(){
-        return "index";
+        return "/index.html";
     }
 }
