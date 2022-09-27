25d13402c10f6f1287f6be80782ecfd5a619824b
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-26 23:11:47 -0500

    Updated Employee class

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,12 +1,14 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EmployeeController {
-    @RequestMapping("employee")
+    @GetMapping("/employee")
     public String employeePage(){
+        
         return "/index.html";
     }
 }
