1cf0e970d86a6702703e3726749a74ccc072cc03
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-27 11:09:44 -0500

    Updated with Thymeleaf

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,14 +1,30 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class EmployeeController {
     @GetMapping("/employee")
-    public String employeePage(){
-        
-        return "/index.html";
+    public String employeePage(Model model){
+        List<String> states = Arrays.asList("Alaska", "Maryland", "California");
+        model.addAttribute("states", states);
+        model.addAttribute("employee", new Employee());
+        return "/register";
     }
+
+    @PostMapping("/employee-list")
+    public String employeeist(@ModelAttribute Employee employee){
+        return "redirect:/employee";
+    }
+
+
 }
